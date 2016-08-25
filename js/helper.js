18a036e9604dc33d0918ae8316b63c9e0cec7d07
ccantey <cantey0701@gmail.com>
2016-08-25 12:03:01 -0500

    active tab selections

--- js/helper.js
+++ js/helper.js
@@ -2,4 +2,22 @@ $( document ).ready(function() {
 	//kickoff map logic
     initialize();
 
+    $('.election-navigation-a').on('click', function(e){
+    	e.preventDefault();
+      //remove previous selections map methods give an example of how one would toggle layers
+      document.getElementById('features').innerHTML = "";
+      // map.removeLayer("2012results-"+ activeTab.geography);
+      // map.removeLayer("2012results-"+ activeTab.geography+"-hover");
+      // map.setLayoutProperty(activeTab.geography + '-symbols', 'visibility', 'none');
+      // map.setLayoutProperty(activeTab.geography + '-lines', 'visibility', 'none');
+    	$('.election-navigation-a').removeClass('active');
+      
+      //add new selections
+    	$(this).addClass('active');
+    	activeTab.selection = $(this).data('district');
+        activeTab.geography = $(this).data('geography');
+        activeTab.name = $(this).data('name');
+    	// changeData(activeTab);
+    })
+
  });
